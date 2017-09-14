a632645da8aa69f4aa57464d5b85a072eb4ea64f
Ryan <ryan.d.elliott@gmail.com>
2017-09-13 18:48:11 -0600

    Add navigation functionality to main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 console.log("Page Loaded");
 
 // Navigation
+var navAbout = document.getElementById("nav-about");
+var navPortfolio = document.getElementById("nav-portfolio");
+var pageAbout = document.getElementById("page-about");
+var pagePortfolio = document.getElementById("page-portfolio");
 
+navAbout.addEventListener("click", function(){
+	pageAbout.setAttribute("style", "display: block");
+	pagePortfolio.setAttribute("style", "display: none");
+
+	navAbout.setAttribute("style", "text-decoration: underline");
+	navPortfolio.setAttribute("style", "text-decoration: none");
+});
+
+navPortfolio.addEventListener("click", function(){
+	pageAbout.setAttribute("style", "display: none");
+	pagePortfolio.setAttribute("style", "display: block");
+
+	navAbout.setAttribute("style", "text-decoration: none");
+	navPortfolio.setAttribute("style", "text-decoration: underline");
+});
 
 // Secret Hacker
 
